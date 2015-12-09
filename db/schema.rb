95b3b365bceb9b97cd173e1a93d36b3a556801c5
David Inga <david.inga@vizzuality.com>
2015-12-09 11:18:21 +0100

    added pagel model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209093621) do
+ActiveRecord::Schema.define(version: 20151209101658) do
 
   create_table "case_studies", force: :cascade do |t|
     t.string   "title",                       null: false
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20151209093621) do
     t.string   "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "title",                  null: false
+    t.text     "body"
+    t.integer  "type",       default: 1
+    t.string   "background"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
