0f8304fdcb964b63cb15c5e2fd48575e085a3312
David Inga <david.inga@vizzuality.com>
2015-12-09 18:45:35 +0100

    added logo validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,5 +5,6 @@ class Contact < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 2 }
 
   has_attached_file :logo, styles: { medium: '230x100>' }
+  validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
 
 end
