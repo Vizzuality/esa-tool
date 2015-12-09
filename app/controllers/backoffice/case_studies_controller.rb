6c4338eb77700e480191cc0eadf6eb52c9699aa8
David Inga <david.inga@vizzuality.com>
2015-12-09 18:41:46 +0100

    flash messages and template as select

--- app/controllers/backoffice/case_studies_controller.rb
+++ app/controllers/backoffice/case_studies_controller.rb
@@ -18,7 +18,7 @@ class Backoffice::CaseStudiesController < BackofficeController
   def create
     @case_study = CaseStudy.new(case_studies_params)
     if @case_study.save
-      redirect_to edit_backoffice_case_study_path(@case_study)
+      redirect_to edit_backoffice_case_study_path(@case_study), notice: 'Case study created successfully.'
     else
       render :new
     end
@@ -26,7 +26,7 @@ class Backoffice::CaseStudiesController < BackofficeController
 
   def update
     if @case_study.update(case_studies_params)
-      redirect_to backoffice_case_studies_path
+      redirect_to backoffice_case_studies_path, notice: 'Case study updated successfully.'
     else
       render :edit
     end
@@ -34,7 +34,7 @@ class Backoffice::CaseStudiesController < BackofficeController
 
   def destroy
     @case_study.destroy
-    redirect_to backoffice_case_studies_path
+    redirect_to backoffice_case_studies_path, notice: 'Case study deleted successfully.'
   end
 
   private
