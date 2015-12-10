5ac9dbe62712d2afbbc19df21272dec38ef11fc1
David Inga <david.inga@vizzuality.com>
2015-12-10 12:27:55 +0100

    select_type page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :backoffice do
     resources :case_studies, path: 'case-studies' do
       resources :pages
+      get 'select_type' => 'select_type#index'
     end
 
     root 'case_studies#index', as: 'dashboard'
