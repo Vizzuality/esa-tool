46172cc40c24998870a1b351913933ebd11e134b
David Inga <davidsingal@icloud.com>
2015-12-10 21:01:49 +0100

    data layer validation

--- app/models/data_layer.rb
+++ app/models/data_layer.rb
@@ -2,4 +2,6 @@ class DataLayer < ActiveRecord::Base
 
   belongs_to :page
 
+  validates :table_name, presence: true
+
 end
--- app/models/interest_point.rb
+++ app/models/interest_point.rb
@@ -2,4 +2,8 @@ class InterestPoint < ActiveRecord::Base
 
   belongs_to :page
 
+  validates :lat, presence: true
+  validates :lng, presence: true
+  validates :distance, presence: true
+
 end
