e564b552fde45d04e0282067da76908b14598f5c
David Inga <david.inga@vizzuality.com>
2015-12-18 11:39:19 +0100

    added case details template images and text page background

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,17 +1,19 @@
 class Page < ActiveRecord::Base
 
+  acts_as_taggable_on :chart_types
+
   belongs_to :case_study
   has_many :data_layers
   has_many :interest_points
-
-  acts_as_taggable_on :chart_types
+  has_attached_file :background, styles: {
+    medium: '385x200#',
+    large: '1920x1080#'
+  }
 
   accepts_nested_attributes_for :data_layers, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :interest_points, reject_if: :all_blank, allow_destroy: true
 
   validates :title, presence: true, length: { minimum: 2 }
-
-  has_attached_file :background, styles: { large: '1920x1080>' }
   validates_attachment_content_type :background, content_type: /\Aimage\/.*\Z/
 
 end
