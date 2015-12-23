1e493a13014d37774613f9d8e0284806ce14d147
David Inga <david.inga@vizzuality.com>
2015-12-23 18:39:57 +0100

    added number of columns

--- db/schema.rb
+++ db/schema.rb
@@ -77,8 +77,11 @@ ActiveRecord::Schema.define(version: 20151221173053) do
 
   create_table "pages", force: :cascade do |t|
     t.string   "title",                               null: false
-    t.text     "body"
+    t.text     "body_first"
+    t.text     "body_second"
+    t.text     "body_thirth"
     t.integer  "page_type",               default: 1
+    t.integer  "text_columns",            default: 1
     t.integer  "color_palette"
     t.string   "custom_color_palette"
     t.integer  "case_study_id",                       null: false
