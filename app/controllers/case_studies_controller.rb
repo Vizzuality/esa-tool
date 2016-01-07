43a375f01934ce9c5a1fc9e962d00f9c1e04df55
David Inga <david.inga@vizzuality.com>
2016-01-07 12:20:51 +0100

    added preview button

--- app/controllers/case_studies_controller.rb
+++ app/controllers/case_studies_controller.rb
@@ -1,11 +1,26 @@
 class CaseStudiesController < ApplicationController
 
+  after_action :check_case_study
+
   def show
     @case_study = CaseStudy.find_published(params[:id])
     gon.case_study = @case_study.to_json(include: [:contacts, :pages])
-    unless @case_study
+  end
+
+  def preview
+    if user_signed_in?
+      @case_study = CaseStudy.find(params[:id])
+    else
       not_found
     end
   end
+
+  private
+
+    def check_case_study
+      unless @case_study
+        not_found
+      end
+    end
 
 end
