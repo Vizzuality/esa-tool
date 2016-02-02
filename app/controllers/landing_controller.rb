50611f22049c3bb7ce848f5b69cec96a564ebaa3
Simao Belchior <simaobelchior@gmail.com>
2016-02-02 15:28:48 +0000

    using ActiveModelSerializers

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -6,29 +6,8 @@ class LandingController < ApplicationController
     @tags = Tag.all
     @case_studies = CaseStudy.limit(9)
 
-    # gon.case_studies = render_to_string json: @case_studies, each_serializer: CaseStudySerializer
-    gon.case_studies = serialize_cases(@case_studies)
+    gon.case_studies = ActiveModel::ArraySerializer.
+      new(@case_studies, each_serializer: CaseStudySerializer).to_json
     gon.cartodb_user = ENV["CDB_USERNAME"]
-
-
   end
-
-  private
-
-  def serialize_cases(cases)
-    Struct.new("CaseSerializedClass", :id, :title, :case_path, :lat, :lng)
-    cases_serialized = []
-    cases.each do |c|
-      case_serialized = Struct::CaseSerializedClass.new(
-        c.id,
-        c.title,
-        case_study_path(c.slug),
-        c.lat,
-        c.lng
-      )
-      cases_serialized << case_serialized
-    end
-    return cases_serialized.to_json
-  end
-
 end
