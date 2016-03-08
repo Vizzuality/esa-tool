863d3cbaccc090a4a3c1749e4726cf7df3f73f9a
Adrián <aloha@adrianperez.info>
2016-03-08 11:23:54 +0100

    fix migration

--- db/migrate/20160307173229_add_raster_color_input.rb
+++ db/migrate/20160307173229_add_raster_color_input.rb
@@ -1,5 +1,5 @@
 class AddRasterColorInput < ActiveRecord::Migration
   def change
-  	add_column :data_layers, :raster_color_input, :string
+  	add_column :data_layers, :raster_color_input, :text, default: 'test'
   end
 end
