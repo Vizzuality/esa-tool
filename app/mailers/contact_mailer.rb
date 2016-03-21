c9f29a3c1bac26368893fbb73656abc9f0432cc1
j8seangel <joseangel.parreno@vizzuality.com>
2016-03-21 19:42:10 +0100

    Add mail template

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,8 @@
 class ContactMailer < ApplicationMailer
   default from: "contact@esaworld.com"
 
+  layout "contact_mailer"
+
   def contact_form(form)
     @form = form
     mail(to: "admin@esaworld.com", subject: 'New submission on esaworld contact form')
